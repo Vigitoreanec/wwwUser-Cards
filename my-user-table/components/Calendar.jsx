5e0379838934e63772aca5f67a1eed6300e4c1b6
Vigitoreanec <chirik39@mail.ru>
2024-10-29 19:02:06 +0200

    удалил ошибку вывода

--- my-user-table/components/Calendar.jsx
+++ my-user-table/components/Calendar.jsx
@@ -12,11 +12,11 @@ const Demo = (() => {
         [value, setValue] = useState(DateToYYYYMM(new Date));
     console.debug('Demo');
     //const dateOpt = useCallback(() => YYYYMMToDate(value), [value]);
-    const dateOpt = YYYYMMToDate(value);
+    //const dateOpt = YYYYMMToDate(value);
 
     return <div>
         <input type='month' value={value} onChange={event => setValue(event.target.value)} />
-        <GetCalendar date={dateOpt} />
+        <GetCalendar date={YYYYMMToDate(value)} />
     </div>
 });
 
@@ -25,7 +25,7 @@ function DateToYYYYMM(date) {
     return date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, '0');
 }
 
-function YYYYMMToDate(str){
+function YYYYMMToDate(str) {
     const [year, month] = str.split('-');
     console.debug('Year to Date');
     return new Date(year, month - 1, 1);
@@ -35,12 +35,12 @@ function YYYYMMToDate(str){
 
 function GetCalendar({ date }) {
     const
-        caption = date.toLocaleDateString('ru', { month: 'long', year: 'numeric' }),
+        caption = (date.toLocaleDateString('ru', { month: 'long', year: 'numeric' })),
         year = date.getFullYear(),
         month = date.getMonth(),
         weekDayName = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
         max = (new Date(year, month + 1, 0)).getDate(),
-        daysInMonth = new Date(year, month + 1, 0).getDate(), // вс=0, пн=1, вт=2.....сб=6
+        daysInMonth = (new Date(year, month, 1)).getDay(), // вс=0, пн=1, вт=2.....сб=6
         shift = (-1 + daysInMonth + 7) % 7,                   //  пн=0, вт=1,.....сб=5, вс=6
 
         dayNames = useMemo(() =>
@@ -53,12 +53,12 @@ function GetCalendar({ date }) {
     return <table>
         <caption>{caption}</caption>
         <thead><tr>{dayNames}</tr></thead>
-        <Month shift={shift} max={max}/>
+        <Month shift={shift} max={max} />
     </table>
 
 };
 
-const Month = memo(function Month({ shift, max }) {
+function Month({ shift, max }) {
     const
         arr = [];
     for (let start = 1 - shift; start <= max; start += 7) {
@@ -66,9 +66,9 @@ const Month = memo(function Month({ shift, max }) {
     }
     console.debug('month');
     return <tbody>{arr}</tbody>;
-});
+};
 
-const Week = memo(function Week({ start, max }) {
+function Week({ start, max }) {
     const
         arr = [];
     for (let day = start; day < start + 7; day++)
@@ -77,4 +77,4 @@ const Week = memo(function Week({ start, max }) {
         </td>);
     console.debug('week');
     return <tr>{arr}</tr>
-});
+};
